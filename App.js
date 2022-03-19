cfae71eb40f655f19979a5b23fb2103531879517
Egemen <egmnydin@gmail.com>
2022-03-19 20:59:13 +0300

    çirkin card

--- App.js
+++ App.js
@@ -1,114 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-// SELAMUN ALEYKÜM
-//aleykum selam
-// sa yine
-import React from 'react';
-import type {Node} from 'react';
+import React,{ useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+
+const App = () => {
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardName}>Kahve masasında kadın eksik</Text>
+          <Text style={styles.cardCategory}>Kahve</Text>
+        </View>
+        <View style={styles.cardBody}>
+          <Text style={styles.cardDesc}>Akşam arabicada toplanıyoruz arkadaşlar. Ama full erkeğiz bi tane karı lazım gelmek isteyen başvuru atsın herkesi kabul ediyoruz.</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  card:{
+    margin:5,
+    backgroundColor:'#000000',
+    borderRadius:10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  cardHeader:{
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  cardName:{
+    paddingLeft:10,
+    paddingTop:5,
+    color: '#ffffff',
+    flex:4,
   },
-  highlight: {
-    fontWeight: '700',
+  cardCategory:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'blue',
+    borderRadius:10,
+    marginRight:7,
+    marginTop:3,
   },
+  cardBody:{
+    padding:15,
+  },
+  cardDesc:{
+    color:'white',
+  }
 });
 
 export default App;
